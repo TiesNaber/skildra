4902b3c55bf3dbc030a1bc6fb61b39d2a35033b1
Ties Naber <tiesnaber@live.nl>
2021-01-23 16:01:25 +0100

    Added quest system scripts

---
+++ SkildraVR/Assets/Scripts/Quest System/QuestController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  -------------- TO DO -------------------------
+///  * Start quest
+///  * Wait for quest to finish
+///  * Complete quest
+///  * Start new Quest
+///  
+///  * Add Actor events
+///     - Talk
+///     - Wait for input
+///     - Animations
+///     - Done
+///     
+///   *Story
+///     - State
+///     - Current Actors
+///     - Current Quest
+///     - Music to play
+///     - Story controller
+///  
+/// </summary>
+
+namespace skildravr.story.quest{ 
+    public class QuestController : MonoBehaviour
+    {
+        
+    }
+}
