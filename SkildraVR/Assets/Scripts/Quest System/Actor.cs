f6d241c84cad746627222f75cc142a7968726c2b
Ties Naber <tiesnaber@live.nl>
2021-01-29 10:13:40 +0100

    Added actor

--- SkildraVR/Assets/Scripts/Quest System/Actor.cs
+++ SkildraVR/Assets/Scripts/Quest System/Actor.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Actor : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+namespace skildravr.story.actor {
+    public class Actor : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
+        private List<ActorActionSO> actorActions;
+        private byte currentActionIndex = 0;
+
+        private void ClearActions(){
+            actorActions.Clear();
+        }
+
+        public void AddAction(ActorActionSO action){
+            if(action == null || actorActions.Count  <= 0){
+                
+                return
+            }
+        }
         
     }
 }
