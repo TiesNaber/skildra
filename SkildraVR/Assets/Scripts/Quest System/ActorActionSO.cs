6a262ec66c7cef547140a95ace5180ba6292d869
Ties Naber <tiesnaber@live.nl>
2021-01-29 10:11:19 +0100

    Actor actions scriptable objects
    Objects that will contain data for creating quests

---
+++ SkildraVR/Assets/Scripts/Quest System/ActorActionSO.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+
+namespace skildravr.story.actor {
+    [CreateAssetMenu(fileName = "ActorActionData", menuName = "ActorActions/ActorActionSO", order = 1)]
+    public class ActorActionSO : ScriptableObject {
+        [Tooltip("Responsible actor for this action")]
+        public Actor actor;
+
+        [Tooltip("Quest index for this action")]
+        public int QuestIndex;
+
+        [Tooltip("Name of the animation that should play for this action")]
+        public string AnimationName;
+
+        [Tooltip("Audio clip that should play during this action")]
+        public AudioClip ScriptClip;
+    }
+}
