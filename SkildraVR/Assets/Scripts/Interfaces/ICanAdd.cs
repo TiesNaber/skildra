84def944fdc4065b01ed4521e0ea76a8ce457356
Ties Naber <tiesnaber@live.nl>
2021-01-29 10:10:24 +0100

    Added CanAdd interface

---
+++ SkildraVR/Assets/Scripts/Interfaces/ICanAdd.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace skildravr.interfaces {
+    interface ICanAdd<T>  {
+
+        bool CanAdd(T item){
+            if(T || J == null)
+        }
+    }
+}
