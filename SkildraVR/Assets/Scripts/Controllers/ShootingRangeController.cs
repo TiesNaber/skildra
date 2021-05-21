65c780a42abec31f8a162107ecc0da571a5721af
Ties <tiesnaber@live.nl>
2021-05-21 05:02:52 -0700

    Made controller folder + made the ShootingRangeController script

---
+++ SkildraVR/Assets/Scripts/Controllers/ShootingRangeController.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using overexcited.vr.weapons.target;
+
+namespace overexcited.vr.controllers{
+
+    public class ShootingRangeController : MonoBehaviour
+    {
+        private byte timesHit;
+        private const byte maxHits = 5;
+
+        [SerializeField]
+        private float endGameTime; // In Minutes!
+        private float maxIdleTime;
+        private bool isIdle;
+
+        private Coroutine idleTimerCoroutine;
+
+        private void OnAwake(){
+            SubscribeToEvents();
+        }
+
+        private void InitInstance(){
+            timesHit = 0;
+            isIdle = false;
+            maxIdleTime = minutesToSeconds();
+        }
+
+        /// <summary>
+        /// Converts the maxIdleTime variable to the amount of seconds
+        /// </summary>
+        /// <returns>idle time in seconds</returns>
+        private float minutesToSeconds(){
+            return endGameTime * 60.0f;
+        }
+
+        private void BeginIdleTimer(){
+            idleTimerCoroutine = StartCoroutine(trackIdleTime());
+        }
+
+        private void StopIdleTimer(){
+            if(idleTimerCoroutine != null){
+                StopCoroutine(idleTimerCoroutine);
+            }
+        }
+
+        private IEnumerator trackIdleTime(){
+        
+            yield return new WaitForSeconds(endGameTime);
+        
+        }
+
+
+        private void TargetHit()
+        {
+            if (!isMaxHitsReached())
+            {
+                timesHit++;
+            }
+            else
+            {
+                throw new System.Exception("SHOULD END SCENE, BUT NOT IMPLEMENTED YET");// Call End Scene
+            }
+        }
+
+        private bool isMaxHitsReached()
+        {
+            if (timesHit < maxHits)
+                return false;
+            else return true;
+        }
+
+        private void SubscribeToEvents(){
+            ShootingTarget.OnHitTarget += TargetHit;
+        }
+        private void UnsubscribeToEvents(){
+            ShootingTarget.OnHitTarget -= TargetHit;
+        }
+
+        private void OnDestroy()
+        {
+            UnsubscribeToEvents();
+        }
+    }
+}
+
