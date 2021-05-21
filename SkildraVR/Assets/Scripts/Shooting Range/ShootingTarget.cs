55fd1bc18d6b300e0a6d345c161e75de7494ec4f
Ties <tiesnaber@live.nl>
2021-05-21 05:00:59 -0700

    Made ShootingTarget script + Shooting Range folder

---
+++ SkildraVR/Assets/Scripts/Shooting Range/ShootingTarget.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using overexcited.vr.weapons.range;
+
+namespace overexcited.vr.weapons.target
+{
+    [RequireComponent(typeof(Collider))]
+    public class ShootingTarget : MonoBehaviour
+    {
+        public delegate void TargetEvent();
+        public static event TargetEvent OnHitTarget;
+
+
+        private bool isHittable;
+
+        private void OnAwake(){
+            isHittable = true;
+        }
+
+        private void TargetHit(){
+            if(OnHitTarget != null){
+                OnHitTarget();
+                isHittable = true;
+            }
+        }
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            var arrow = other.GetComponent<Arrow>();
+            if(arrow != null && isHittable){
+                isHittable = false;
+                TargetHit();
+            }
+        }
+    }
+}
