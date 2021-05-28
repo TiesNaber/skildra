141aa5356a0e5ac19d89e937ecd174456cbe8c2b
Ties <tiesnaber@live.nl>
2021-05-28 05:26:39 -0700

    Added Generic Singleton Script

---
+++ SkildraVR/Assets/Scripts/General/Singleton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Component
+{
+    public static T sharedInstance { get; private set; }
+    public virtual void Awake()
+    {
+        if (sharedInstance != null && sharedInstance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            sharedInstance = this as T;
+        }
+    }
+}
